7b11cb7a76838aff668b85791914af567da836a5
Francesco Bigagnoli <francesco.bigagnoli@scmitaly.it>
2017-11-14 18:57:16 +0100

    AlbumTableViewCell created

---
+++ Pellicola/Classes/Views/AlbumTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  AlbumTableViewCell.swift
+//  Pellicola
+//
+//  Created by francesco bigagnoli on 14/11/2017.
+//
+
+import UIKit
+
+class AlbumTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var albumTitle: UILabel!
+    @IBOutlet weak var photosCount: UILabel!
+    @IBOutlet weak var albumThumb: UIImageView!
+}
