0bf29d63e6a3e6a5b3df11eba849c89f9d073e33
Francesco Bigagnoli <francesco.bigagnoli@scmitaly.it>
2017-11-15 12:42:16 +0100

    Album cell with title and photos count

--- Pellicola/Classes/AlbumsViewController.swift
+++ Pellicola/Classes/AlbumsViewController.swift
@@ -10,13 +10,12 @@ import UIKit
 import Photos
 
 public final class AlbumsViewController: UIViewController {
-    @IBOutlet var tableView: UITableView!
-    
     private static let smartAlbumSubtypes: [PHAssetCollectionSubtype]  = [.smartAlbumUserLibrary,
                                                                           .smartAlbumFavorites,
                                                                           .smartAlbumSelfPortraits,
                                                                           .smartAlbumScreenshots,
                                                                           .smartAlbumPanoramas]
+    @IBOutlet var tableView: UITableView!
     private var albums: [PHAssetCollection] = []
     
     override public func viewDidLoad() {
@@ -74,17 +73,58 @@ extension AlbumsViewController {
     private func configureUI() {
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(actionDismiss))
         title = NSLocalizedString("albums.title", bundle:  Bundle(for: AlbumsViewController.self), comment: "")
+        
+        tableView.register(UINib(nibName: "AlbumTableViewCell", bundle: Bundle(for: AlbumTableViewCell.self)), forCellReuseIdentifier: "AlbumCell")
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 85
+    }
+    
+    private func configuredCell(forAlbum album: PHAssetCollection, atIndex indexPath: IndexPath) -> UITableViewCell {
+        guard let albumCell = tableView.dequeueReusableCell(withIdentifier: "AlbumCell", for: indexPath) as? AlbumTableViewCell else {
+            assertionFailure("Error dequeuing album cell")
+            return UITableViewCell()
+        }
+        
+        if let albumTitle = album.localizedTitle {
+            albumCell.albumTitle.text = albumTitle
+        } else {
+            assertionFailure("You should provide an album placeholder title")
+        }
+        
+        //Photos Count
+        let assetsFetch = PHAsset.fetchAssets(in: album, options: nil)
+        let numOfImages = assetsFetch.countOfAssets(with: .image)
+        albumCell.photosCount.text = String(numOfImages)
+        
+        //Album thumbnail
+        let keyImageFetch = PHAsset.fetchKeyAssets(in: album, options: nil)
+        if let thumbAsset = keyImageFetch?.firstObject ?? (assetsFetch.firstObject ?? nil) {
+            let imageManager = PHImageManager()
+            let requestOptions = PHImageRequestOptions()
+            requestOptions.deliveryMode = .fastFormat
+            requestOptions.isNetworkAccessAllowed = true
+            let targetSize = 
+            imageManager.requestImage(for: thumbAsset, targetSize: CGSizeScale(albumCell.albumThumb.frame.size, UIScreen.main.scale), contentMode: .default, options: requestOptions, resultHandler: { (image, info) in
+                albumCell.albumThumb.image = image
+            })
+            
+        } else {
+            assertionFailure("You should provide an album placeholder thumbnail")
+        }
+        
+        return albumCell
     }
 }
 
 //MARK: UITableViewDataSource
 extension AlbumsViewController: UITableViewDataSource {
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return albums.count
     }
     
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let album = albums[indexPath.row]
+        return configuredCell(forAlbum: album, atIndex: indexPath)
     }
 }
 
