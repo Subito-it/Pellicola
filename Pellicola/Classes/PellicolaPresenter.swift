535ffecb33ca85b7c96deac11f437c8a0ff37c4c
Francesco Bigagnoli <francesco.bigagnoli@scmitaly.it>
2018-02-15 14:34:33 +0100

    Method extraction refactorings

--- Pellicola/Classes/PellicolaPresenter.swift
+++ Pellicola/Classes/PellicolaPresenter.swift
@@ -38,39 +38,47 @@ public final class PellicolaPresenter: NSObject {
     @objc public func present(on presentingViewController: UIViewController, maxNumberOfSelections: Int) {
         
         PHPhotoLibrary.requestAuthorization { [weak self] (status) in
-            guard case .authorized = status else {
-                let alert = UIAlertController(title: NSLocalizedString("alert_access_denied.title", bundle: Bundle.framework, comment: ""),
-                                              message: NSLocalizedString("alert_access_denied.message", bundle: Bundle.framework, comment: ""),
-                                              preferredStyle: .alert)
-                let okAction = UIAlertAction(title: NSLocalizedString("alert_access_denied.later", bundle: Bundle.framework, comment: ""), style: .default, handler: { [weak self] _ in
-                    self?.userDidCancel?()
-                })
-                alert.addAction(okAction)
+            DispatchQueue.main.async {
+                guard let sSelf = self else { return }
+                guard case .authorized = status else {
+                    let alert = sSelf.openSettingsAlert()
+                    presentingViewController.present(alert, animated: true)
+                    return
+                }
                 
-                let settingsAction = UIAlertAction(title: NSLocalizedString("alert_access_denied.settings", bundle: Bundle.framework, comment: ""), style: .cancel, handler: { _ in
-                   UIApplication.shared.openURL(URL(string: UIApplicationOpenSettingsURLString)!)
-                })
-                alert.addAction(settingsAction)
-                
-                presentingViewController.present(alert, animated: true)
-                return
+                sSelf.setupPresenter(with: maxNumberOfSelections)
+                guard let assetCollectionsVC = sSelf.createAssetsCollectionViewController() else { return }
+                sSelf.configureNavigationController(with: [assetCollectionsVC])
+                presentingViewController.present(sSelf.navigationController, animated: true, completion: nil)
             }
-            
-            guard let sSelf = self else { return }
-            
-            sSelf.setupPresenter(with: maxNumberOfSelections)
-            
-            guard let assetCollectionsVC = sSelf.createAssetsCollectionViewController() else { return }
-            sSelf.navigationController.toolbar.tintColor = sSelf.style.blackColor
-            sSelf.navigationController.toolbar.barTintColor = sSelf.style.toolbarBackgroundColor
-            sSelf.navigationController.setViewControllers([assetCollectionsVC], animated: false)
-            sSelf.navigationController.modalPresentationStyle = .formSheet
-            presentingViewController.present(sSelf.navigationController, animated: true, completion: nil)
         }
+    }
         
+    // MARK: - UI Configuration
+    private func configureNavigationController(with viewControllers:[UIViewController]) {
+        navigationController.toolbar.tintColor = style.blackColor
+        navigationController.toolbar.barTintColor = style.toolbarBackgroundColor
+        navigationController.modalPresentationStyle = .formSheet
+        navigationController.setViewControllers(viewControllers, animated: false)
     }
     
     // MARK: - Helper method
+    
+    private func openSettingsAlert() -> UIAlertController {
+        let alert = UIAlertController(title: NSLocalizedString("alert_access_denied.title", bundle: Bundle.framework, comment: ""),
+                                      message: NSLocalizedString("alert_access_denied.message", bundle: Bundle.framework, comment: ""),
+                                      preferredStyle: .alert)
+        let okAction = UIAlertAction(title: NSLocalizedString("alert_access_denied.later", bundle: Bundle.framework, comment: ""), style: .default, handler: { [weak self] _ in
+            self?.userDidCancel?()
+        })
+        alert.addAction(okAction)
+        
+        let settingsAction = UIAlertAction(title: NSLocalizedString("alert_access_denied.settings", bundle: Bundle.framework, comment: ""), style: .cancel, handler: { _ in
+            UIApplication.shared.openURL(URL(string: UIApplicationOpenSettingsURLString)!)
+        })
+        alert.addAction(settingsAction)
+        return alert
+    }
     
     private func setupPresenter(with maxNumberOfSelections: Int) {
         dataStorage = DataStorage(limit: maxNumberOfSelections <= 0 ? nil : maxNumberOfSelections)
