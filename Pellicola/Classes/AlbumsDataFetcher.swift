7f46e3dfd61031b043b427ff8558341e09fff4e4
Francesco Bigagnoli <francesco.bigagnoli@scmitaly.it>
2018-05-15 15:32:29 +0200

    Dispatch completion handler to main thread

--- Pellicola/Classes/AlbumsDataFetcher.swift
+++ Pellicola/Classes/AlbumsDataFetcher.swift
@@ -21,20 +21,28 @@ class AlbumsDataFetcher {
             album.photoCount = fetchedAssets.count
             
             guard let lastAsset = fetchedAssets.lastObject else {
-                completion (nil)
+                DispatchQueue.main.async {
+                    completion (nil)
+                }
+                
                 return
             }
             
             let options = PHImageRequestOptions()
             options.isNetworkAccessAllowed = true
             options.deliveryMode = .fastFormat
+            //From Apple's doc
+            /* By default, this method executes asynchronously. If you call it from a background thread you may change the isSynchronous property of the options parameter to true to block the calling thread until either the requested image is ready or an error occurs, at which time Photos calls your result handler */
+            options.isSynchronous = true
             
             self?.cachingImageManager.requestImage(for: lastAsset,
                                                    targetSize: size,
                                                    contentMode: .aspectFill,
                                                    options: options,
                                                    resultHandler: { (image, _) in
-                                                    completion(image)
+                                                    DispatchQueue.main.async {
+                                                        completion(image)
+                                                    }
             })
         }
         
